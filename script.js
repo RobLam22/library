3a432b49e83e484358e752682b1480a0f028ae92
Robert Lam <robert.lam22@gmail.com>
2024-10-03 17:29:32 +1000

    skelly files

---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
